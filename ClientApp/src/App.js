497538a65918e23c42eda649a1bc42f500cc4643
kleecollage <56179611+kleecollage@users.noreply.github.com>
2023-05-04 17:18:59 -0500

    update 04/05/23
    Formulario y generación de una tabla que muestra tres datos insertados por el cliente mediante el uso de Hooks

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,126 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
-import './custom.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
 
-export default class App extends Component {
-  static displayName = App.name;
+const App = () => {
+    const [fechaInicial, setFechaInicial] = useState("");
+    const [fechaFinal, setFechaFinal] = useState("");
+    const [diaCorte, setDiaCorte] = useState("");
 
-  render() {
+    const handleGenerarTabla = (e) => {
+        e.preventDefault();
+        if (fechaInicial && fechaFinal && diaCorte) {
+            const tbody = document.querySelector("#tablaFacturacion tbody");
+            const row = document.createElement("tr");
+            const fechaInicialCell = document.createElement("td");
+            fechaInicialCell.textContent = fechaInicial;
+            const fechaFinalCell = document.createElement("td");
+            fechaFinalCell.textContent = fechaFinal;
+            const diaCorteCell = document.createElement("td");
+            diaCorteCell.textContent = diaCorte;
+            row.appendChild(fechaInicialCell);
+            row.appendChild(fechaFinalCell);
+            row.appendChild(diaCorteCell);
+            tbody.appendChild(row);
+        }
+    };
+
+
+    const handleFechaInicialChange = (event) => {
+        setFechaInicial(event.target.value);
+    };
+
+    const handleFechaFinalChange = (event) => {
+        setFechaFinal(event.target.value);
+    };
+
+    const handleDiaCorteChange = (event) => {
+        setDiaCorte(event.target.value);
+    };
+
     return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
-    );
-  }
+        <div className="row">
+            <div className="col-sm-6">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="card-title">Calendario de facturacion</h2>
+                        <form>
+                            <div className="p-3">
+                                <label htmlFor="FechaInicial" className="form-label">Fecha Inicial</label>
+                                <input type="date" className="form-control" id="FechaInicial" value={fechaInicial} onChange={handleFechaInicialChange}></input>
+                            </div>
+
+                            <div className="p-3">
+                                <label htmlFor="FechaFinal" className="form-label">Fecha Final</label>
+                                <input type="date" className="form-control" id="FechaFinal" value={fechaFinal} onChange={handleFechaFinalChange}></input>
+                            </div>
+
+                            <div className="input-group my-4">
+                                <div className="input-group-prepend">
+                                    <label className="input-group-text" htmlFor="Periodicidad">Periodicidad</label>
+                                </div>
+                                <select className="custom-select" id="Periodicidad">
+                                    <option defaultValue>Elegir</option>
+                                    <option value="1">Mensual</option>
+                                    <option value="2">Bimestral</option>
+                                    <option value="3">Semestral</option>
+                                    <option value="4">Anual</option>
+                                </select>
+                            </div>
+
+                            <div className="p-3">
+                                <label htmlFor="DiaCorte" className="form-label">Dia de Corte</label>
+                                <input type="text" className="form-control" id="DiaCorte" value={diaCorte} onChange={handleDiaCorteChange}></input>
+                            </div>
+
+                            <div className="p-3">
+                                <label htmlFor="ImpresionFactura" className="form-label">Dia de impresion</label>
+                                <input type="text" className="form-control" id="ImpresionFactura"></input>
+                            </div>
+
+                            <div className="p-3 text-center">
+                                <button
+                                    type="button"
+                                    className="btn btn-lg btn-primary col-sm-6"
+                                    id="Generar"
+                                    onClick={handleGenerarTabla}
+                                >Generar</button>
+                            </div>
+
+                        </form>
+
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="col-sm-6">
+                <div className="card">
+                    <table id="tablaFacturacion">
+                        {diaCorte && (
+                            <thead>
+                                <tr>
+                                    <th>Fecha inicial</th>
+                                    <th>Fecha final</th>
+                                    <th>Dia de corte</th>
+                                </tr>
+                            </thead>
+                        )}
+                        <tbody>
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+
+
+
+        </div >
+
+                    
+
+    )
+
 }
+
+export default App                        
